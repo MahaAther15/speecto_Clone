125418534024ac0dc8f7aa35520ac257711524ab
MahaAther15 <mahaather45@gmail.com>
2026-07-10 21:52:51 +0500

    fix: set 50px gap between Hero and How do we work section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,17 +16,21 @@ const Home = () => {
   return (
     <div className="min-h-screen relative bg-white dark:bg-darkBg text-slate-900 dark:text-white transition-colors duration-300">
       <Navbar />
-      <div className="flex flex-col gap-[50px] md:gap-[80px]">
+      <div className="flex flex-col">
         <Hero />
-        <Work />
-        <Requirement />
-        <ProductDesign />
-        <SystemDesign />
-        <Development />
-        <Testing />
-        <Production />
-        <Deployment />
-        <ContactUs />
+        <div className="pt-[50px]">
+          <Work />
+        </div>
+        <div className="flex flex-col gap-[50px] md:gap-[80px] mt-[50px] md:mt-[80px]">
+          <Requirement />
+          <ProductDesign />
+          <SystemDesign />
+          <Development />
+          <Testing />
+          <Production />
+          <Deployment />
+          <ContactUs />
+        </div>
       </div>
       <Footer />
     </div>
